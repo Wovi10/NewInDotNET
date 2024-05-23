b41926ef113e8dea88066c870da6a09f7736f92c
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-05-23 08:50:12 +0200

    Starting collection part

---
+++ ConsolePart/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Frozen;
+using System.Collections.Immutable;
+
+var list = new List<int> {1, 2, 2, 3};
+
+
+#region HashSet
+
+var hashSet = list.ToHashSet();
+
+#endregion
+
+
+#region ImmutableHashSet
+
+var immutableHashSet = list.ToImmutableHashSet();
+
+#endregion
+
+#region FrozenSet
+
+var frozenSet = list.ToFrozenSet();
+
+#endregion
+
+
+
+Console.WriteLine();
